07f5aa071341f637a9596fe6566ba448cb45c70d
Nothingtop <henrycbj@hotmail.com>
2018-01-27 21:46:36 -0500

    6 tests

--- src/test/CustomTests.java
+++ src/test/CustomTests.java
@@ -12,11 +12,115 @@ import org.junit.Test;
 public class CustomTests extends TestCase {
 
     private KVStore kvClient;
+    private static int CLIENT_CONNECTIONS = 50;
+
+    @Before
+    public void setUp() {
+        kvClient = new KVStore("localhost", 50000);
+        try {
+            kvClient.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        kvClient.disconnect();
+    }
 
     @Test
-    public void testPut() {
-        String key = "foo2";
-        String value = "bar2";
+    public void testMultipleConnections() {
+        Exception ex = null;
+
+        try {
+            for (int i=0; i < CLIENT_CONNECTIONS; i++) {
+                KVStore kvClient = new KVStore("localhost", 50000);
+                kvClient.connect();
+            }
+
+        } catch (Exception e) {
+            ex = e;
+        }
+
+        assertNull(ex);
+    }
+
+    @Test
+    public void testDoubleUpdate() {
+        String key = "updateThisTwice";
+        String value1 = "one";
+        String value2 = "two";
+        String value3 = "three";
+        KVMessage response = null;
+        Exception ex = null;
+
+        try {
+            response = kvClient.put(key, value1);
+            response = kvClient.put(key, value2);
+            response = kvClient.put(key, value3);
+        } catch (Exception e) {
+            ex = e;
+        }
+
+        Assert.assertTrue(ex == null && response.getValue().equals(value3));
+    }
+
+    @Test
+    public void testGetNonExistant() {
+        String key = "thisDoesNotExist";
+        KVMessage response = null;
+        Exception ex = null;
+
+        try {
+            response = kvClient.get(key);
+        } catch (Exception e) {
+            ex = e;
+        }
+
+        Assert.assertTrue(ex == null && response.getStatus() == StatusType.GET_ERROR);
+    }
+
+    @Test
+    public void testGetAfterDelete() {
+        String key = "deleteAndGet";
+        String value = "init";
+        KVMessage response = null;
+        Exception ex = null;
+
+        try {
+            response = kvClient.put(key, value);
+            response = kvClient.put(key, "");
+            response = kvClient.get(key);
+        } catch (Exception e) {
+            ex = e;
+        }
+
+        Assert.assertTrue(ex == null && response.getStatus() == StatusType.GET_ERROR);
+    }
+
+    @Test
+    public void testDoubleDelete() {
+        String key = "thisDoesNotExist";
+        String value ="deleteThisTwice";
+        KVMessage response = null;
+        Exception ex = null;
+
+        try {
+            response = kvClient.put(key, value);
+            response = kvClient.put(key, "");
+            response = kvClient.put(key, "");
+        } catch (Exception e) {
+            ex = e;
+        }
+
+        Assert.assertTrue(ex == null && response.getStatus() == StatusType.DELETE_ERROR);
+    }
+
+    @Test
+    public void testDeleteNonExistant() {
+        String key = "thisDoesNotExist";
+        String value = "";
         KVMessage response = null;
         Exception ex = null;
 
@@ -26,6 +130,8 @@ public class CustomTests extends TestCase {
             ex = e;
         }
 
-        Assert.assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+        Assert.assertTrue(ex == null && response.getStatus() == StatusType.DELETE_ERROR);
     }
+
+
 }
