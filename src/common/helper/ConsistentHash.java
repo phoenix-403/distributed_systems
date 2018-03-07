77c97ef5fbe86ccbbefcb13e22450569ee929395
zcc1202 <zc.chew@mail.utoronto.ca>
2018-03-07 02:09:28 -0500

    consistent hashing fixed

--- src/common/helper/ConsistentHash.java
+++ src/common/helper/ConsistentHash.java
@@ -1,16 +1,16 @@
 package common.helper;
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.security.*;
+import java.math.*;
+import java.util.*;
+import common.helper.IDHashPair;
 
 public class ConsistentHash {
-    public String getMD5(String key) {
+    private TreeMap<String, String[]> idHashRangePairs = new TreeMap<String, String[]>();
+    public String getMD5(String preImage) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] messageDigest = md.digest(key.getBytes("UTF-8"));
+            byte[] messageDigest = md.digest(preImage.getBytes("UTF-8"));
             BigInteger number = new BigInteger(1, messageDigest);
             System.out.println("number: " + number);
             StringBuilder hashText = new StringBuilder(number.toString(16));
@@ -26,35 +26,60 @@ public class ConsistentHash {
         }
     }
 
-
-    public static IDHashPair getServerResponsible(IDHashPair tupleHashed, ArrayList<IDHashPair> idHashPairs){
-        int index = 0;
-        for(int i = 0; i < idHashPairs.size(); i++){
-            if(tupleHashed.compareTo(idHashPairs.get(i)) < 0)
-                index = i%idHashPairs.size();
+    public void storeHashRange(ArrayList<IDHashPair> idHashPairs) {
+            Collections.sort(idHashPairs);
+            for (int i = 0; i < idHashPairs.size(); i++) {
+                String[] hashRanges = new String[2];
+                if (i > 0) {
+                    BigInteger value = new BigInteger(idHashPairs.get(i-1).getHashVal(), 16);
+                    value = value.add(BigInteger.ONE);
+                    StringBuilder start = new StringBuilder(value.toString(16));
+                    // Now we need to zero pad it if you actually want the full 32 chars.
+                    while (start.length() < 32) {
+                        start.insert(0, "0");
+                    }
+                    hashRanges[0] = start.toString();
+                    hashRanges[1] = idHashPairs.get(i).getHashVal();
+                } else {
+                    if(idHashPairs.get(idHashPairs.size()-1).getHashVal().equals("ffffffffffffffffffffffffffffffff")){
+                        hashRanges[0] = "00000000000000000000000000000001";
+                        hashRanges[1] = idHashPairs.get(i).getHashVal();
+                    }
+                    else {
+                        BigInteger value = new BigInteger(idHashPairs.get(idHashPairs.size() - 1).getHashVal(), 16);
+                        value = value.add(BigInteger.ONE);
+                        StringBuilder start = new StringBuilder(value.toString(16));
+                        // Now we need to zero pad it if you actually want the full 32 chars.
+                        while (start.length() < 32) {
+                            start.insert(0, "0");
+                        }
+                        hashRanges[0] = start.toString();
+                        // wraparound
+                        hashRanges[1] = idHashPairs.get(i).getHashVal();
+                    }
+                }
+                idHashRangePairs.put(idHashPairs.get(i).getId(), hashRanges);
+            }
         }
-        return idHashPairs.get(index);
-    }
 
     public static void main(String[] args) {
         ConsistentHash testHash = new ConsistentHash();
+        TreeMap<String, String []> serverHashRange = new TreeMap<String, String []>();
         ArrayList<IDHashPair> idHashPairs = new ArrayList<IDHashPair>();
 
 //        System.out.println(testHash.getMD5("testing shit"));
         IDHashPair test1 = new IDHashPair("server1", testHash.getMD5("test1"));
         idHashPairs.add(test1);
+
         IDHashPair test2 = new IDHashPair("server2", testHash.getMD5("test2"));
         idHashPairs.add(test2);
+
         IDHashPair test0 = new IDHashPair("server0", testHash.getMD5("test0"));
         idHashPairs.add(test0);
-//        IDHashPair tuple0 = new IDHashPair("tuple0", testHash.getMD5("test3"));
-//        idHashPairs.add(tuple0);
-        IDHashPair tuple0 = new IDHashPair("tuple0", "f1ffffffffffffffffffffffffffffff");
-        idHashPairs.add(tuple0);
-        Collections.sort(idHashPairs);
-        IDHashPair serverResponsible = getServerResponsible(tuple0, idHashPairs);
-        idHashPairs.remove(test0);
-        Collections.sort(idHashPairs);
-        IDHashPair serverResponsible2 = getServerResponsible(tuple0, idHashPairs);
+
+        IDHashPair test3 = new IDHashPair("server3", "ffffffffffffffffffffffffffffffff");
+        idHashPairs.add(test3);
+
+        testHash.storeHashRange(idHashPairs);
     }
 }
---
+++ src/test/IECSNodeTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
+import ecs.ECSNode;
+
+
+//public class IECSNodeTest {
+//    public void setUp() {
+//        Exception ex = null;
+//        try {
+//            ecsNode = new ECSNode("server1","nagger",9999,"f1ffffffffffffffffffffffffffffff", false);
+//        } catch (Exception e) {
+//            ex = e;
+//        }
+//    }
+//}
