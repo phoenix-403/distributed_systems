ff430faea59607fc1e60d406d16dbfc24790ba55
zcc1202 <zc.chew@mail.utoronto.ca>
2018-03-07 23:05:11 -0500

    fixed consistent hash - 1 server case

--- src/common/helper/ConsistentHash.java
+++ src/common/helper/ConsistentHash.java
@@ -66,25 +66,18 @@ public class ConsistentHash {
     }
 
     private void storeHashRange() {
-        Collections.sort(idHashPairs);
-        for (int i = 0; i < idHashPairs.size(); i++) {
+        if(idHashPairs.size() == 1){
             String[] hashRanges = new String[2];
-            if (i > 0) {
-                BigInteger value = new BigInteger(idHashPairs.get(i - 1).getHashVal(), 16);
-                value = value.add(BigInteger.ONE);
-                StringBuilder start = new StringBuilder(value.toString(16));
-                // Now we need to zero pad it if you actually want the full 32 chars.
-                while (start.length() < 32) {
-                    start.insert(0, "0");
-                }
-                hashRanges[0] = start.toString();
-                hashRanges[1] = idHashPairs.get(i).getHashVal();
-            } else {
-                if (idHashPairs.get(idHashPairs.size() - 1).getHashVal().equals("ffffffffffffffffffffffffffffffff")) {
-                    hashRanges[0] = "00000000000000000000000000000001";
-                    hashRanges[1] = idHashPairs.get(i).getHashVal();
-                } else {
-                    BigInteger value = new BigInteger(idHashPairs.get(idHashPairs.size() - 1).getHashVal(), 16);
+            hashRanges[0] = "00000000000000000000000000000000";
+            hashRanges[1] = idHashPairs.get(0).getHashVal();
+            idHashRangePairs.put(idHashPairs.get(0).getId(), hashRanges);
+        }
+        else {
+            Collections.sort(idHashPairs);
+            for (int i = 0; i < idHashPairs.size(); i++) {
+                String[] hashRanges = new String[2];
+                if (i > 0) {
+                    BigInteger value = new BigInteger(idHashPairs.get(i - 1).getHashVal(), 16);
                     value = value.add(BigInteger.ONE);
                     StringBuilder start = new StringBuilder(value.toString(16));
                     // Now we need to zero pad it if you actually want the full 32 chars.
@@ -92,31 +85,35 @@ public class ConsistentHash {
                         start.insert(0, "0");
                     }
                     hashRanges[0] = start.toString();
-                    // wraparound
                     hashRanges[1] = idHashPairs.get(i).getHashVal();
+                } else {
+                    if (idHashPairs.get(idHashPairs.size() - 1).getHashVal().equals("ffffffffffffffffffffffffffffffff")) {
+                        hashRanges[0] = "00000000000000000000000000000000";
+                        hashRanges[1] = idHashPairs.get(i).getHashVal();
+                    } else {
+                        BigInteger value = new BigInteger(idHashPairs.get(idHashPairs.size() - 1).getHashVal(), 16);
+                        value = value.add(BigInteger.ONE);
+                        StringBuilder start = new StringBuilder(value.toString(16));
+                        // Now we need to zero pad it if you actually want the full 32 chars.
+                        while (start.length() < 32) {
+                            start.insert(0, "0");
+                        }
+                        hashRanges[0] = start.toString();
+                        // wraparound
+                        hashRanges[1] = idHashPairs.get(i).getHashVal();
+                    }
                 }
+                idHashRangePairs.put(idHashPairs.get(i).getId(), hashRanges);
             }
-            idHashRangePairs.put(idHashPairs.get(i).getId(), hashRanges);
         }
     }
 
     public static void main(String[] args) {
-//        ConsistentHash testHash = new ConsistentHash();
-//        TreeMap<String, String[]> serverHashRange = new TreeMap<String, String[]>();
-//        ArrayList<IDHashPair> idHashPairs = new ArrayList<IDHashPair>();
-//
-//        IDHashPair test1 = new IDHashPair("server1", testHash.getMD5("test1"));
-//        idHashPairs.add(test1);
-//
-//        IDHashPair test2 = new IDHashPair("server2", testHash.getMD5("test2"));
-//        idHashPairs.add(test2);
-//
-//        IDHashPair test0 = new IDHashPair("server0", testHash.getMD5("test0"));
-//        idHashPairs.add(test0);
-//
-//        IDHashPair test3 = new IDHashPair("server3", "ffffffffffffffffffffffffffffffff");
-//        idHashPairs.add(test3);
-//
-//        testHash.storeHashRange(idHashPairs);
+        ECSNode serv1 = new ECSNode("server1", "chewzi", 9999,null , true);
+        ArrayList<ECSNode> ecsNodes = new ArrayList<ECSNode>();
+        ecsNodes.add(serv1);
+        ConsistentHash testHash =  new ConsistentHash(ecsNodes);
+        testHash.hash();
+
     }
 }
